696777895e18740e8b5d37b69c496e5cdfb649f7
Krzysztof Rycombel <krzysztofr@tutamail.com>
2020-03-07 23:23:57 +0100

    improved algorithm 2

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     private static int fibonacci(int n) {
         if (n < 2) return n;
-        return fibonacci(n-1) * n;
+        return fibonacci(n-1) + fibonacci(n-2);
     }
 
     public static void main (String[] args) {
