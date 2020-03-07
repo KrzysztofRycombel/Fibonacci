5cc78f2b5626d069d142b66c12868438246f8d3a
Krzysztof Rycombel <krzysztofr@tutamail.com>
2020-03-07 23:08:19 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+/*
+Program znajduje n-ty wyraz ciągu Fibonacciego.
+*/
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    private static int fibonacci(int n) {
+        if (n < 2) return n;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+    public static void main (String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Podaj n-ty wyraz ciagu Fibonacciego: ");
+
+        try {
+            int nStringNumber = input.nextInt();
+            System.out.println("Wynik: "+fibonacci(nStringNumber));
+
+        } catch (InputMismatchException e){
+            System.out.println("Podaj liczbę w prawidłowym formacie");
+        }
+
+
+    }
+}
